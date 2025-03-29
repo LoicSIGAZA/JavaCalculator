54847e11a971cbd69984cedb6bfdb877f6e9de97
Loic <loicsigning10@gmail.com>
2025-03-29 19:28:29 +0000

    added new tests on java file

--- src/test/java/com/example/softwaretesting_22805969/CalculatorLogicTest.java
+++ src/test/java/com/example/softwaretesting_22805969/CalculatorLogicTest.java
@@ -31,4 +31,49 @@ class CalculatorLogicTest {
     void divideByZero() {
         assertThrows(ArithmeticException.class, () -> calc.divide(30, 0));
     }
+
+    @Test
+    void testAddition10() {
+        assertEquals(10, calc.add(5, 5));
+    }
+
+    @Test
+    void testDivisionBy2() {
+        assertEquals(5, calc.divide(10, 2));
+    }
+
+    @Test
+    void testNegativeSubtraction() {
+        assertEquals(-5, calc.subtract(5, 10));
+    }
+
+    @Test
+    void testZeroMultiplication() {
+        assertEquals(0, calc.multiply(0, 99));
+    }
+
+    @Test
+    void testLargeAddition() {
+        assertEquals(1000000, calc.add(999999, 1));
+    }
+
+    @Test
+    void testDivideByZeroThrowsException() {
+        assertThrows(ArithmeticException.class, () -> calc.divide(10, 0));
+    }
+    @Test
+    void testConditionalAddition() {
+        int a = 10;
+        int b = 25;
+        int result = calc.add(a, b);
+
+        if (result == 35) {
+            System.out.println("Passed: 10 + 25 = 35");
+            assertEquals(35, result);
+        } else {
+            System.out.println("Failed: 10 + 25 != 35");
+            fail("Addition logic failed");
+        }
+    }
+
 }
